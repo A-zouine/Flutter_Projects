2030ef18bd8a9d9f0c54a06b2679a8af034eb86f
azouine <azouine@gmail.com>
2023-05-15 23:32:35 +0100

    new fille update

--- onboarding_screen/lib/main.dart
+++ onboarding_screen/lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed this button this many times if you need that:',
+              'You have pushed this button this many times if you i need :',
             ),
             Text(
               '$_counter',
