6352400858ed2885a3fb48e8d1e3578d1af64d4b
azouine <azouine@gmail.com>
2023-05-15 23:15:36 +0100

    this commit for test 2

--- onboarding_screen/lib/main.dart
+++ onboarding_screen/lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed this button this many times:',
+              'You have pushed this button this many times if you need that:',
             ),
             Text(
               '$_counter',
